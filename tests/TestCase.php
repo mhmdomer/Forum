42311719b083b6d49a32859cd9040f97a5eaa324
m7md3omer <mohammedomer789@gmail.com>
2020-05-13 08:22:16 +0200

    DO NOT WIPE THE DATABASE AFTER RUNNIG CONFIG:CACHE git add .git add . solved now

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,6 +2,7 @@
 
 namespace Tests;
 
+use Illuminate\Contracts\Console\Kernel;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
@@ -13,5 +14,33 @@ abstract class TestCase extends BaseTestCase
         $user = $user? : create('App\User');
         $this->actingAs($user);
         return $this;
+    }
+
+    /**
+     * Creates the application.
+     *
+     * @return \Illuminate\Foundation\Application
+     */
+    public function createApplication()
+    {
+        $app = require __DIR__.'/../bootstrap/app.php';
+
+        $app->make(Kernel::class)->bootstrap();
+
+        $this->clearCache(); // NEW LINE -- Testing doesn't work properly with cached stuff.
+
+        // these are to refresh configs and environment variables, since $app has loaded cache before it was cleared
+        $app->make(\Illuminate\Foundation\Bootstrap\LoadEnvironmentVariables::class)->bootstrap($app);
+        $app->make(\Illuminate\Foundation\Bootstrap\LoadConfiguration::class)->bootstrap($app);
+
+        return $app;
+    }
+
+    protected function clearCache()
+    {
+        $commands = ['clear-compiled', 'cache:clear', 'view:clear', 'config:clear', 'route:clear'];
+        foreach ($commands as $command) {
+            \Illuminate\Support\Facades\Artisan::call($command);
+        }
     }
 }
